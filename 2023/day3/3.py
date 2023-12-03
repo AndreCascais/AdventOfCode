b7efa07914aa29bf2474e5c7a7310a4c7eb96445
AndreCascais <a_cascais@hotmail.com>
2023-12-03 19:59:45 +0000

    Update day3 solution

--- 2023/day3/3.py
+++ 2023/day3/3.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import sys
 import re
+import math
 
 document = sys.stdin.read().splitlines()
 document = [f".{line}." for line in document]
@@ -13,7 +14,7 @@ document.insert(len(document), dummyLine)
 count = 0
 for lineIndex, line in enumerate(document):
     for match in re.finditer("\\d+", line):
-        span = (match.span()[0] - 1, match.span()[1] + 1)
+        span = (match.start() - 1, match.end() + 1)
         above = [x for y in document[lineIndex - 1][span[0]:span[1]] for x in y]
         below = [x for y in document[lineIndex + 1][span[0]:span[1]] for x in y]
         horizontal = [document[lineIndex][span[0]], document[lineIndex][span[1] - 1]]
@@ -22,19 +23,17 @@ for lineIndex, line in enumerate(document):
 
 print(count)
 
-count = 0
 partNumberDic = defaultdict(list)
 for lineIndex, line in enumerate(document):
     for match in re.finditer("\\d+", line):
-        span = (match.span()[0] - 1, match.span()[1] + 1)
+        span = (match.start() - 1, match.end() + 1)
         above = [(lineIndex - 1, col) for col in range(span[0], span[1])]
         below = [(lineIndex + 1, col) for col in range(span[0], span[1])]
         horizontal = [(lineIndex,span[0]), (lineIndex, span[1] - 1)]
-        asterix = [(row, col)for (row, col) in above + below + horizontal if document[row][col] == "*"]
+        asterix = [(row, col) for (row, col) in above + below + horizontal if document[row][col] == "*"]
         for ast in asterix:
-            partNumberDic[ast].append(match.group())
+            partNumberDic[ast].append(int(match.group()))
 gears = [numbers for numbers in partNumberDic.values() if len(numbers) == 2]
-print(sum([int(first) * int(second) for first, second in gears]))
-
-
+ratioSums = sum([math.prod(gear) for gear in gears])
+print(ratioSums)
 
