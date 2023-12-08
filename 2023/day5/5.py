c6103700f3c5f8e271d05524e7a712740e6c1a31
AndreCascais <a_cascais@hotmail.com>
2023-12-08 18:39:35 +0000

    Add day5 pt 2 solution

--- 2023/day5/5.py
+++ 2023/day5/5.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
+from functools import reduce
 import sys
 import re
+import portion as P
 
 data = [line.splitlines() for line in sys.stdin.read().split("\n\n")]
 
@@ -10,6 +12,7 @@ categoryToIntervals = defaultdict(list)
 for mapIndex, maps in enumerate(data[1:]):
     for map in maps[1:]:
         categoryToIntervals[mapIndex].append([int(entry) for entry in map.split()])
+
 
 def getLastCategory(seed):
     mapKey = seed
@@ -23,17 +26,45 @@ def getLastCategory(seed):
             mapKey,
         )
     return mapKey
-seedMap = [getLastCategory(seed) for seed in seeds]
-
-minPart1 = min(seedMap)
-print(minPart1)
-
-minPart2 = getLastCategory(seeds[0])
-# Brute force wont work
-# for seedIndex in range(0, len(seeds), 2):
-#     print(f"seraching {seedIndex} out of {len(seeds)/2}")
-#     for seed in range(seeds[seedIndex], seeds[seedIndex] + seeds[seedIndex + 1]):
-#         minPart2 = min(minPart2, getLastCategory(seed))
-print(minPart2)
 
 
+locations = [getLastCategory(seed) for seed in seeds]
+
+print(f"Part1: {min(locations)}")
+
+seedRanges = reduce(
+    lambda x, y: x | y,
+    [
+        P.closedopen(begin, begin + length)
+        for begin, length in zip(seeds[::2], seeds[1::2])
+    ],
+    P.empty(),
+)
+
+
+def getLastCategoryIntervals(intervals):
+    for intervalMaps in categoryToIntervals.values():
+        mappedIntervals = P.empty()
+        for destBegin, srcBegin, length in intervalMaps:
+            sourceInterval = P.closedopen(srcBegin, srcBegin + length)
+            offset = destBegin - srcBegin
+            intersections = [
+                sourceInterval.intersection(interval)
+                for interval in intervals
+                if sourceInterval.intersection(interval) != P.empty()
+            ]
+            for intersection in intersections:
+                intervals -= intersection
+                intervalLength = intersection.upper - intersection.lower
+                mappedInterval = P.closedopen(
+                    intersection.lower + offset,
+                    intersection.lower + offset + intervalLength,
+                )
+                mappedIntervals |= mappedInterval
+        # intervals that dont have a map carry over to next section
+        intervals |= mappedIntervals
+    return intervals
+
+
+locationIntervals = getLastCategoryIntervals(seedRanges)
+print(f"min part2: {locationIntervals.lower}")
