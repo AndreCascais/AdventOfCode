e79bbf262fd505456c15c62333d9f9802fed776b
AndreCascais <a_cascais@hotmail.com>
2023-12-15 20:17:28 +0000

    Add day15.py

---
+++ 2023/day15/15.py
@@ -0,0 +1,36 @@
+import sys
+from functools import reduce
+import re
+
+sequence = sys.stdin.read().splitlines()[0].split(",")
+
+
+def hash(step):
+    return reduce(lambda total, element: ((total + ord(element)) * 17) % 256, step, 0)
+
+
+hashSum = sum([hash(step) for step in sequence])
+
+print(hashSum)
+
+d = {}
+boxes = {box: {} for box in range(256)}
+for step in sequence:
+    op = re.split("=|-", step.strip("-"))
+    label = op[0]
+    box = hash(label)
+    # - case
+    if len(op) == 1:
+        if label in boxes[box]:
+            del boxes[box][label]
+    # = case
+    else:
+        boxes[box][label] = int(op[1])
+
+focusingPowers = [
+    (boxIdx + 1) * (slot + 1) * length
+    for boxIdx, box in boxes.items()
+    for slot, length in enumerate(box.values())
+]
+
+print(sum(focusingPowers))
