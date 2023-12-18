03b0c60e0bb30f7c4b73c7b6a4a5887ab1ec8d2e
AndreCascais <a_cascais@hotmail.com>
2023-12-18 23:42:05 +0000

    Add day 17 solution

--- 2023/day17/17.py
+++ 2023/day17/17.py
@@ -4,34 +4,11 @@ import queue
 
 heatLossMap = [list(map(int, line)) for line in sys.stdin.read().splitlines()]
 
-start = (0, 0)
-end = (len(heatLossMap) - 1, len(heatLossMap[0]) - 1)
-
-
 nodes = [
     (row, col) for row in range(len(heatLossMap)) for col in range(len(heatLossMap[0]))
 ]
 
 dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-edges = defaultdict(list)
-
-for node in nodes:
-    x, y = node
-    for dir in dirs:
-        dx, dy = dir
-        nx = x + dx
-        ny = y + dy
-        for steps in range(1, 3):
-            edges[(node, dir, steps)].append(((nx, ny), dir, steps + 1))
-
-        rotations = [(-abs(dy), -abs(dx)), (abs(dy), abs(dx))]
-        for rx, ry in rotations:
-            lx = x + rx
-            ly = y + ry
-            for steps in range(1, 4):
-                edges[(node, dir, steps)].append(((lx, ly), (rx, ry), 1))
-
 
 ultraEdges = defaultdict(list)
 
@@ -41,60 +18,48 @@ for node in nodes:
         dx, dy = dir
         nx = x + dx
         ny = y + dy
-        for steps in range(1, 10):
-            if not (
-                (nx <= 0 and dx == -1)
-                or (nx >= len(heatLossMap) - 1 and dx == 1)
-                or (ny <= 0 and dy == -1)
-                or (ny >= len(heatLossMap[0]) - 1 and dy == 1)
+        for steps in range(11):
+            if (nx, ny) != (
+                len(heatLossMap) - 1,
+                len(heatLossMap[0]) - 1,
             ) or steps >= 4:
                 ultraEdges[(node, dir, steps)].append(((nx, ny), dir, steps + 1))
 
         rotations = [(-abs(dy), -abs(dx)), (abs(dy), abs(dx))]
         for rx, ry in rotations:
-            lx = x + rx
-            ly = y + ry
-            maxX = x + 4 * rx
-            maxY = y + 4 * ry
-            # if (maxX <= 0 or maxX >= len(heatLossMap) - 1 or maxY <= 0 or maxY >= len(heatLossMap[0]) - 1):
             for steps in range(4, 11):
-                ultraEdges[(node, dir, steps)].append(((lx, ly), (rx, ry), 1))
+                ultraEdges[(node, dir, steps)].append(((x, y), (rx, ry), 0))
 
 
-def dijkstra(edges=edges):
+def dijkstra(edges):
     visited = set()
     distances = {node: sys.maxsize for node in edges.keys()}
-    start = ((0, 0), (0, 1), 1)
-    distances[start] = 0
-    prev = {}
+    startDirs = [(0, 1), (1, 0)]
     q = queue.PriorityQueue()
-    q.put((0, start))
+    for dir in startDirs:
+        start = ((0, 0), dir, 0)
+        distances[start] = 0
+        q.put((0, start))
     while not q.empty():
         prio, node = q.get()
-        # print(f"Checking node {node} with prio {prio}")
         if node[0] == (len(heatLossMap) - 1, len(heatLossMap[0]) - 1):
-            print(f"dist is {prio} for node {node}")
-            return (node, prev)
+            return prio
 
         visited.add(node)
         for neighbor in edges[node]:
-            # print(f"Checking neighbor {neighbor}")
             if neighbor in visited:
                 continue
             try:
-                updatedDist = prio + heatLossMap[neighbor[0][0]][neighbor[0][1]]
-                if updatedDist < distances[neighbor]:
+                updatedDist = prio + (
+                    0
+                    if neighbor[2] == 0
+                    else heatLossMap[neighbor[0][0]][neighbor[0][1]]
+                )
+                if updatedDist <= distances[neighbor]:
                     distances[neighbor] = updatedDist
                     q.put((updatedDist, neighbor))
-                    prev[neighbor] = node
             except:
                 pass
 
 
-# last, prevDic = dijkstra()
-# 889 -- last guess - too high
-last, prevDic = dijkstra(ultraEdges)
-
-# while last[0] != (0, 0):
-#     print(f"{last} with weight {heatLossMap[last[0][0]][last[0][1]]}")
-#     last = prevDic[last]
+print(dijkstra(ultraEdges))
